224603ba75578ef9c8969b1ec24d71ee280a966f
jayaj-j <124872973+jayaj-j@users.noreply.github.com>
2023-03-10 00:37:35 +0530

    Update selenium_test.py

--- selenium_test.py
+++ selenium_test.py
@@ -25,7 +25,7 @@ def browser():
 
 # Test step 1 - Open URL
 def test_open_url(browser):
-    browser.get("http://13.235.69.201/")
+    browser.get("http://65.1.108.7/")
 
 
 # Test step 2 - Check Title
